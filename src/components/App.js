e838895f815080dbc7de85af06557dff5696a6ec
richie1988 <richiessikaonga@gmail.com>
2023-09-20 21:01:25 +0200

    Fetched the data from the Api and rendered the quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calculator from './calculator';
 import './calculator.css';
+import QuoteDisplay from './QuoteDisplay';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <QuoteDisplay />
     </div>
   );
 }
